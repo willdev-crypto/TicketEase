243ece31c874356bc5d43edb4cb6ef450911601f
willdev-crypto <wil.dgl@hotmail.com>
2025-07-02 13:17:45 -0300

    Cache [aberturachamados.html](http://_vscodecontentref_/1) no service worker

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,12 @@ const urlsToCache = [
   "/index.html",
   "/styles.css",
   "/script.js",
+  "/aberturachamados.html", 
   "/assets/web-app-manifest-192x192.png",
   "/assets/web-app-manifest-512x512.png"
 ];
 
-// Instalar o Service Worker e armazenar os arquivos no cache
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -17,7 +18,7 @@ self.addEventListener("install", event => {
   );
 });
 
-// Ativar o Service Worker e remover caches antigos
+
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -32,7 +33,7 @@ self.addEventListener("activate", event => {
   );
 });
 
-// Interceptar requisições e buscar no cache
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
